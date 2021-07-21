7fd9b3d7283bcc8e0781d87a36c06176e4b91e1c
dimasabi <87745486+dimasabi@users.noreply.github.com>
2021-07-21 13:18:02 +0700

    Add files via upload

---
+++ ganjilgenap.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$bil=10; // Inisialisasi variabel bil dengan nilai 10
+
+if ($bil % 2 == 0){ //Kondisi
+    echo " Genap"; //Kondisi true
+}else {
+    echo " Ganjil"; //Kondisi false
+}
+?>
